45992a8d548dc0f4da099307e1514cc05a577e26
GhifariArsa <s3893099@student.rmit.edu.au>
2024-01-16 13:55:11 +0700

    development env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'ghifariarsa',
+      password: 'arsa125690',
+      database: 'soundvault',
+      entities: [],
+      synchronize: true,
+    }),
+    ConfigModule.forRoot(),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
