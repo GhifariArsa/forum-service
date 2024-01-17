52679c0978b0df1a0777975a9d3e837b1caf5c20
GhifariArsa <s3893099@student.rmit.edu.au>
2024-01-17 18:27:24 +0700

    comment and discussion Upvote

--- src/forum/service/upvote/upvote.service.ts
+++ src/forum/service/upvote/upvote.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUpvoteParams } from 'src/forum/utils/types';
 import { Comments } from 'src/typeorm/entity/Comments';
@@ -22,13 +22,22 @@ export class UpvoteService {
 
   async createUpvote(upvoteParams: CreateUpvoteParams) {
     const discussionId = upvoteParams.discussionId;
+    const commentId = upvoteParams.commentId;
 
+    // Checking if Discussion or Comment is present
     if (discussionId !== undefined && discussionId !== null) {
-      // Assuming that discussionId is valid, you may want to add validation here
-
       const discussion = await this.discussionRepo.findOneBy({
         id: discussionId,
       });
+
+      // Check if user already upvoted
+      const discussionPresent = await this.upvoteRepo.findOne({
+        where: { discussion: discussion, userId: upvoteParams.userId },
+      });
+
+      if (discussionPresent) {
+        throw new HttpException('User already Upvoted', HttpStatus.BAD_REQUEST);
+      }
 
       if (discussion) {
         // If the discussion is found, create and save the upvote
@@ -41,11 +50,34 @@ export class UpvoteService {
         return this.upvoteRepo.save(upvote);
       } else {
         // Discussion not found, handle accordingly
-        throw new Error('Discussion not found');
+        throw new HttpException('Discussion not found', HttpStatus.BAD_REQUEST);
+      }
+    } else if (commentId !== undefined && commentId !== null) {
+      const comment = await this.commentsRepo.findOneBy({ id: commentId });
+
+      // Check if user already upvoted
+      const commentPresent = await this.upvoteRepo.findOne({
+        where: { discussion: comment, userId: upvoteParams.userId },
+      });
+
+      if (commentPresent) {
+        throw new HttpException('User already Upvoted', HttpStatus.BAD_REQUEST);
+      }
+
+      if (comment) {
+        const upvote = this.upvoteRepo.create({
+          userId: upvoteParams.userId,
+          comment: comment,
+          createdAt: new Date(),
+        });
+
+        return this.upvoteRepo.save(upvote);
       }
     } else {
-      // discussionId is not present in upvoteParams
-      throw new Error('Discussion ID is missing in upvoteParams');
+      throw new HttpException(
+        'Discussion or Comment not found',
+        HttpStatus.BAD_GATEWAY,
+      );
     }
   }
 }
