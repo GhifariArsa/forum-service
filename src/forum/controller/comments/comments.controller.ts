f8a8786ad8b8ae50261a8148977dbee2fd10d75f
GhifariArsa <s3893099@student.rmit.edu.au>
2024-01-17 19:04:01 +0700

    read for comments

--- src/forum/controller/comments/comments.controller.ts
+++ src/forum/controller/comments/comments.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { CommentsService } from 'src/forum/service/comments/comments.service';
 
-@Controller('comments')
-export class CommentsController {}
+@Controller('v1/comments')
+export class CommentsController {
+  constructor(private commentService: CommentsService) {}
+
+  @Get()
+  getAllComments() {
+    return this.commentService.getAllComments();
+  }
+
+  @Get(':id')
+  getCommentsForDiscussion(@Param('id', ParseIntPipe) id: number) {
+    return this.commentService.getCommentsFromDiscussion(id);
+  }
+}
--- src/forum/service/comments/comments.service.ts
+++ src/forum/service/comments/comments.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Comments } from 'src/typeorm/entity/Comments';
+import { Discussions } from 'src/typeorm/entity/Discussion';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+  constructor(
+    @InjectRepository(Comments)
+    private commentRepo: Repository<Comments>,
+    @InjectRepository(Discussions)
+    private discussionRepo: Repository<Discussions>,
+  ) {}
+
+  getAllComments() {
+    return this.commentRepo.find();
+  }
+
+  async getCommentsFromDiscussion(discussionId: number) {
+    const discussion = await this.discussionRepo.findOne({
+      where: { id: discussionId },
+    });
+
+    return this.commentRepo.find({ where: { discussion: discussion } });
+  }
+}
